03bb6c5ef91dda05f3da3c7a1242109fb8a50166
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-12-20 17:26:50 +0100

    snack4 done

--- snack4.php
+++ snack4.php
@@ -9,6 +9,19 @@
     <title>snack4</title>
 </head>
 <body>
-    
+    <?php
+    $arraynum = [];
+    for($i=0; count($arraynum)<15; $i++){
+        $num = rand(0,15);
+    if(!in_array($num, $arraynum)){
+        $arraynum[] = $num;
+        echo $arraynum[$i]."<br>";
+    }else{
+        $i--;
+    }; 
+   
+    }
+
+    ?>
 </body>
 </html>
